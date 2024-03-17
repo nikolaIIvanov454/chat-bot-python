99877197c4e4d2009be0644d80ed1a2a22d12358
Nikolai Ivanov <nikitoi_vanov@abv.bg>
2024-03-17 17:14:06 +0200

    commit

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ import keras
 
 import json
 import numpy as np
+import pandas as pd
 
 json_file = open('data.json', 'r')
 
-json_data = json.load(json_file)
+json_data = json.load(json_file)['data']
 
 question_list = []
-
 answers_list = []
+type_list = []
+
+type_list.append(json_data[0]['type'])
+question_list.append(json_data[0]['questions'])
 
 for index in range(0, len(json_data)):
-    question_list.append(json_data[index]['question'])
-    answers_list.append(json_data[index]['answer'])
+    if index % 2 != 0:
+        type_list.append(json_data[index]['type'])
+        answers_list.append(json_data[index]['answers'])
 
-model = keras.Sequential(
-    [
-        keras.layers.Input(shape=(1, )),
-        keras.layers.Dense(32, activation='relu'),
-        keras.layers.Dense(1)
-    ]
-)
+print(data)
 
-model.compile(optimizer='adam', loss='mse')
+# print(question_list, answers_list, type_list)
 
-model.fit(np.array(question_list), np.array(answers_list), epochs=10, batch_size=64)
+# model = keras.Sequential(
+#     [
+#         keras.layers.Input(shape=(1, )),
+#         keras.layers.Dense(32, activation='relu'),
+#         keras.layers.Dense(1)
+#     ]
+# )
 
-result = model.predict(np.array(["What is the sun's color?"], ["What is my name?"]))
+# model.compile(optimizer='adam', loss='mse')
 
-print(result)
+# model.fit(np.array(question_list), np.array(answers_list), epochs=10, batch_size=64)
+
+# result = model.predict(np.array(["What is the sun's color?"], ["What is my name?"]))
+
+# print(result)
